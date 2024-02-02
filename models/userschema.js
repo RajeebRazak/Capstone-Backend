b62e4879abbc03a364c0f3e5e02bd6451349a8b8
Rajeeb Razak <rajeebrazak786@gmail.com>
2024-02-02 22:50:19 +0530

    update

--- models/userschema.js
+++ models/userschema.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
      enum: ["user", "admin"],
      default: "user",
   },
-  PhoneNumber: {
+  Phonenumber: {
      type: String || Number ,
      require: true,
   }
